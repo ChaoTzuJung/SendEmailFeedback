0c6ac2ea247d5d7fcf2e90d156ecaec0861e1109
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-10-10 18:37:11 +0800

    fix sth for heroku

--- services/passport.js
+++ services/passport.js
@@ -10,8 +10,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-	  .then(user => {
+	User.findById(id).then(user => {
 		  done(null, user);
 		});
 });
@@ -34,6 +33,6 @@ passport.use(
 							.then(user => done(null, user));
 					}
 				})		
-		},
+		}
 	)
 );
