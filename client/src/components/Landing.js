7bff60930a2ef848d0818cf5f47406990df90d36
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-10-18 00:59:09 +0800

    Add Billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>
+        Hahow!
+      </h1>
+      Collect feedback form yours users
+    </div>
+  );
+};
+
+export default Landing;
